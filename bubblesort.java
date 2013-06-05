783788a30c3d4bf41dce61c1a9ff1eb4f09f5d80
Peter.Keomanivanh <pkeoman17@gmail.com>
2013-06-05 10:01:57 -0700

    modify a bit more

--- bubblesort.java
+++ bubblesort.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class bubblesort{
 
 	public static int [] unsorted = new int[200000];
-
+	public static int [] unsorted2 = new int[20000];
+	public static int [] unsorted3 = new int[2000];
 	public static void sort(int[] num)
 	{
 		boolean swapped = true;
@@ -46,44 +47,85 @@ public class bubblesort{
 		int n = 42;
 		int MAX = 10;
 		int count =0;
-		double [] best = new double[MAX];
 		double [] time = new double[MAX];
+		double [] best = new double[MAX];
 		
+		double [] timeA = new double[MAX];
+		double [] bestA = new double[MAX];
+		
+		double [] time_20000 = new double[MAX];
+		double [] best_20000 = new double[MAX];
+		
+		double [] time_2000000 = new double[MAX];
+		double [] best_2000000 = new double[MAX];
 		//initialize array;
 		
 		for(int x = 0; x < MAX; x++)
 		{
 			init();
 			System.out.println("Before: ");
-		//	print_array();	
+		//	print_array();		
 			long t0 = System.currentTimeMillis();
 			sort(unsorted);
 			long t1 = System.currentTimeMillis();
 			time[x] = (t1-t0)/1000.0;
+			timeA[x] = t1 - t0;
 			
 			if(isVerify(unsorted) == false)
 			{
 				count++;
 			}	
-			//best case
 			long t2 = System.currentTimeMillis();
 			sort(unsorted);
 			long t3 = System.currentTimeMillis();
 			best[x] = (t3-t2)/1000.0;
+			bestA[x] = t3-t2;
+		//20,000 elements	
 			System.out.println("After: ");
+			 t2 = System.currentTimeMillis();
+			sort(unsorted2);
+			 t3 = System.currentTimeMillis();
+			time_20000[x] = (t3-t2);
+			 t2 = System.currentTimeMillis();
+			sort(unsorted2);
+			 t3 = System.currentTimeMillis();
+			best_20000[x] = (t3-t2);
+		//2,000,000
+			 t2 = System.currentTimeMillis();
+			sort(unsorted3);
+			 t3 = System.currentTimeMillis();
+			time_2000000[x] = (t3-t2);
+			 t2 = System.currentTimeMillis();
+			sort(unsorted3);
+			 t3 = System.currentTimeMillis();
+			best_2000000[x] = (t3-t2);
 		//	print_array();
 			reset_array();
 		}
 
 		 System.out.println("The results:");
-		 System.out.println("Avg   Best");
+		 System.out.println("Average Best");
 		for(int i = 0; i < MAX; i++)
 		{
-		 	System.out.println(time[i] +" " +best[i] );
+		 	System.out.println(time[i] + " "+ best[i]);
+		}
+		
+		 System.out.println("In millisecond");
+		for(int i = 0; i < MAX; i++)
+		{
+		 	System.out.println(timeA[i] + " "+ bestA[i]);
+		}
+		 System.out.println("The results 20000:");
+		for(int i = 0; i < MAX; i++)
+		{
+		 	System.out.println(time_20000[i] + " "+ best_20000[i]);
+		}
+		System.out.println("The results 2000:");
+		for(int i = 0; i < MAX; i++)
+		{
+		 	System.out.println(time_2000000[i] + " "+ best_2000000[i]);
 		}
 		System.out.println("The times unsorted: " + count);
-		
-		
 	}
 	public static void init()
 	{
@@ -93,6 +135,11 @@ public class bubblesort{
 		{
 			unsorted[i] = gen.nextInt(n);	
 		}	
+		for(int i = 0; i < unsorted2.length; i++)
+		{
+			unsorted2[i] = gen.nextInt(n);
+		}
+		
 
 	}
 	
--- insertion.java
+++ insertion.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class insertion{
 
 	public static int [] unsorted = new int[200000];
-
+	public static int [] unsorted2 = new int[20000];
+	public static int [] unsorted3 = new int[2000];
 	public static void sort(int[] num)
 	{
 		int value;
@@ -47,17 +48,28 @@ public class insertion{
 		int count =0;
 		double [] time = new double[MAX];
 		double [] best = new double[MAX];
+		
+		double [] timeA = new double[MAX];
+		double [] bestA = new double[MAX];
+		
+		double [] time_20000 = new double[MAX];
+		double [] best_20000 = new double[MAX];
+		
+		double [] time_2000000 = new double[MAX];
+		double [] best_2000000 = new double[MAX];
 		//initialize array;
 		
 		for(int x = 0; x < MAX; x++)
 		{
 			init();
 			System.out.println("Before: ");
-		//	print_array();	
+		//	print_array();		
 			long t0 = System.currentTimeMillis();
 			sort(unsorted);
 			long t1 = System.currentTimeMillis();
 			time[x] = (t1-t0)/1000.0;
+			timeA[x] = t1 - t0;
+			
 			if(isVerify(unsorted) == false)
 			{
 				count++;
@@ -66,7 +78,26 @@ public class insertion{
 			sort(unsorted);
 			long t3 = System.currentTimeMillis();
 			best[x] = (t3-t2)/1000.0;
+			bestA[x] = t3-t2;
+		//20,000 elements	
 			System.out.println("After: ");
+			 t2 = System.currentTimeMillis();
+			sort(unsorted2);
+			 t3 = System.currentTimeMillis();
+			time_20000[x] = (t3-t2);
+			 t2 = System.currentTimeMillis();
+			sort(unsorted2);
+			 t3 = System.currentTimeMillis();
+			best_20000[x] = (t3-t2);
+		//2,000,000
+			 t2 = System.currentTimeMillis();
+			sort(unsorted3);
+			 t3 = System.currentTimeMillis();
+			time_2000000[x] = (t3-t2);
+			 t2 = System.currentTimeMillis();
+			sort(unsorted3);
+			 t3 = System.currentTimeMillis();
+			best_2000000[x] = (t3-t2);
 		//	print_array();
 			reset_array();
 		}
@@ -76,6 +107,22 @@ public class insertion{
 		for(int i = 0; i < MAX; i++)
 		{
 		 	System.out.println(time[i] + " "+ best[i]);
+		}
+		
+		 System.out.println("In millisecond");
+		for(int i = 0; i < MAX; i++)
+		{
+		 	System.out.println(timeA[i] + " "+ bestA[i]);
+		}
+		 System.out.println("The results 20000:");
+		for(int i = 0; i < MAX; i++)
+		{
+		 	System.out.println(time_20000[i] + " "+ best_20000[i]);
+		}
+		System.out.println("The results 2000:");
+		for(int i = 0; i < MAX; i++)
+		{
+		 	System.out.println(time_2000000[i] + " "+ best_2000000[i]);
 		}
 		System.out.println("The times unsorted: " + count);
 		
@@ -89,7 +136,17 @@ public class insertion{
 		{
 			unsorted[i] = gen.nextInt(n);	
 		}	
+		
+		for (int i= 0; i < unsorted2.length; i++)
+		{
+			unsorted2[i] = gen.nextInt(n);	
+		}	
 
+
+		for (int i= 0; i < unsorted3.length; i++)
+		{
+			unsorted3[i] = gen.nextInt(n);	
+		}	
 	}
 	
 	public static void reset_array()
@@ -97,6 +154,10 @@ public class insertion{
 		for(int i= 0; i < unsorted.length; i++)
 		{
 			unsorted[i] = 0;
+		}
+		for(int i= 0; i < unsorted2.length; i++)
+		{
+			unsorted2[i] = 0;
 		}
 	}
 	public static void print_array()
